df95dbaa65c197fb89ff6919b437d693e62fd61d
Armando Andini <armando.andini@hotmail.com>
2014-11-26 11:47:44 -0300

    Authentication and authorization methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  attr_reader :current_user
+
+  respond_to :json
+
+  private
+
+  def authenticate
+    @current_user = authenticate_with_http_token { |token, options| User.find_by_token token }
+    unless @current_user
+      render json: "Invalid Token", status: 401
+    end
+  end
+
+  def authorize_admin
+    unless @current_user.try :admin?
+      render json: "Only administrators can add books.", status: 401
+    end
+  end
 end
