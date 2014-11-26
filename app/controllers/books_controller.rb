120e687b9eb6f7e7a1e6d28e5329fe727137201c
Armando Andini <armando.andini@hotmail.com>
2014-11-26 11:47:30 -0300

    Clean up controller. Implement index, create and borrow actions.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,39 @@
 class BooksController < ApplicationController
-  # GET /books
-  # GET /books.json
+  before_action :authenticate
+  before_action :authorize_admin, only: [ :create ]
+
   def index
-    @books = Book.all
-    render json: @books
+    if params[:query]
+      @books = Book.search params[:query]
+      render json: @books
+    else
+      render json: "Missing query parameter.", status: 400
+    end
   end
 
-  # GET /books/1
-  # GET /books/1.json
-  def show
-    @book = Book.find(params[:id])
-
-    render json: @book
-  end
-
-  # POST /books
-  # POST /books.json
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.new(book_params)
 
     if @book.save
-      render json: @book, status: :created, location: @book
+      render json: @book, status: 201
     else
-      render json: @book.errors, status: :unprocessable_entity
+      render json: @book.errors, status: 400
     end
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
-  def update
-    @book = Book.find(params[:id])
-
-    if @book.update(book_params)
-      head :no_content
+  def borrow
+    @book = Book.find params[:id]
+    if @book.try :available?
+      @book.borrow!
     else
-      render json: @book.errors, status: :unprocessable_entity
+      render json: "Book is either unavailable or doesn't exist", status: 404
     end
-  end
-
-  # DELETE /books/1
-  # DELETE /books/1.json
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-
-    head :no_content
   end
 
   private
-    
-    def book_params
-      params.require(:book).permit(:name, :year, :author, :borrowed_date, :owner_id)
-    end
+
+  def book_params
+    params.require(:book).permit(:name, :year, :author)
+  end
+
 end
