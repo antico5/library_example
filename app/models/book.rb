c31b1bf49a7972f1f3d33ca2dac0a9a02e2f61e6
Armando Andini <armando.andini@hotmail.com>
2014-11-26 11:46:49 -0300

    Book model. Scopes and instance methods.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,21 @@
 class Book < ActiveRecord::Base
+  belongs_to :owner, class: User, foreign_key: :owner_id
+
+  validates :name, presence: true
+  validates :author, presence: true
+  validates :year, presence: true
+
+  scope :available, -> { where("borrowed_date < ? OR borrowed_date IS NULL", 7.days.ago) }
+
+  def self.search query
+    available.where("name like ?", "%#{ query }%")
+  end
+
+  def available?
+    borrowed_date < 7.days.ago || borrowed_date.nil?
+  end
+
+  def borrow!
+    update( borrowed_date: DateTime.now )
+  end
 end
